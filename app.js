52017d9478b360c42194aa75343f9a6108ad9d79
FraanMoore <fraanmoore@gmail.com>
2022-12-28 02:04:21 +0000

    comentarios de como se podría hacer...no me resulto nada

--- app.js
+++ app.js
@@ -1,30 +1,46 @@
-Array = Nombre  = prompt("Ingresar nombre jugador A");
-Array = Nombre  = prompt("Ingresar nombre jugador B");
 
-let JugadorA = prompt("JugardorA");
-let JugadorB = prompt("JugardorB");
 
-if(JugadorA == "piedra" && JugadorB == "tijera") 
-{console.log("piedra gana a tijera, gana jugador: " + JugadorA);
-} 
-else if(JugadorA == "tijera" && JugadorB == "papel") 
-{console.log("tijera gana a papel, gana jugador: " + JugadorA)}
+Array = Nombre1 = prompt("Nombre jugador A");
+Array = Nombre2 = prompt("Nombre jugador B");
 
-else if(JugadorA == "papel" && JugadorB == "piedra") 
-{console.log("papel gana a piedra, gana jugador: " + JugadorA)}
+let JugadorA = prompt("Ingrese juego JugadorA");
+let JugadorB = prompt("Ingrese juego JugadorB");
+let gana;
+let veces;
+//let x = 1;
+//let suma = 0;
+//let valor;
+//while (x+1)
+//{valor=prompt('¿Cuántas partidas?:','');
+//valor=parseInt(valor);
+//suma=suma+valor;
+//x=x+1;}
+//document.write('La suma de los valores es '+suma+'');
 
-else if(JugadorA == "piedra" && JugadorB == "papel") 
-{console.log("papel gana a piedra, gana jugador: " + JugadorB)}
+//function calcResult (JugadorA, JugadorB){
 
-else if(JugadorA == "tijera" && JugadorB == "piedra") 
-{console.log("piedra gana a tijera, gana jugador: " + JugadorB)}
+if (JugadorA == "piedra" && JugadorB == "tijera") {
+    console.log("piedra gana a tijera, gana jugador: " + JugadorA);
+}
+else if (JugadorA == "tijera" && JugadorB == "papel") { console.log("tijera gana a papel, gana jugador: " + JugadorA) }
 
-else if(JugadorA == "papel" && JugadorB == "tijera") 
-{console.log("tijera gana a papel, gana jugador: " + JugadorB)}
+else if (JugadorA == "papel" && JugadorB == "piedra") { console.log("papel gana a piedra, gana jugador: " + JugadorA) }
+
+else if (JugadorA == "piedra" && JugadorB == "papel") { console.log("papel gana a piedra, gana jugador: " + JugadorB) }
+
+else if (JugadorA == "tijera" && JugadorB == "piedra") { console.log("piedra gana a tijera, gana jugador: " + JugadorB) }
+
+else if (JugadorA == "papel" && JugadorB == "tijera") { console.log("tijera gana a papel, gana jugador: " + JugadorB) }
 
 
 else if (JugadorA == JugadorB) {
-    console.log("Empate")}
+    console.log("Empate")
+}
+
+
+
+
+
 
     //acumulador para el conteo de veces ganadas o empates
     // ejemplo let perdidasJugadorA = 0
